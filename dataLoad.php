6fa61718d70812721dd091c431655d45ec79d033
MeRaj Rudba <mrudba@gmail.com>
2020-05-18 13:25:49 +0600

    Online Cinema Hall management system

--- dataLoad.php
+++ dataLoad.php
@@ -1,19 +1,15 @@
 <?php
-    session_start();
-    if (isset($_GET["movieName"])) {
-        $movieName=$_GET["movieName"];
+session_start();
+if (isset($_GET["movieName"])) {
+    if (!isset($_SESSION["username"])) {
+        header("Location: login.php");
+    } else {
+        $movieName = $_GET["movieName"];
         $_SESSION['movieName'] = $movieName;
         header("Location: booking.php");
-    
     }
-
-    elseif (isset($_GET["noticeToDelete"])) {
-        $noticeTitleToDelete=$_GET["noticeToDelete"];
-        $_SESSION['noticeToDelete'] = $noticeTitleToDelete;
-        header("Location: deleteNotice.php");
-    
-    }
-
-
-
-?>
+} elseif (isset($_GET["noticeToDelete"])) {
+    $noticeTitleToDelete = $_GET["noticeToDelete"];
+    $_SESSION['noticeToDelete'] = $noticeTitleToDelete;
+    header("Location: deleteNotice.php");
+}
